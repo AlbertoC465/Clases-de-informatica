f1a6bd076cd00536cf9d3a276118e947fa1bd9b6
Alberto Cabrera <e12151@ism.edu.ec>
2026-04-14 12:37:17 -0500

    cambios 3

--- actividad 2 (variable).py
+++ actividad 2 (variable).py
@@ -1,1 +1,21 @@
-print("pruebaa")
+print(len("alberto cabrera"))
+longitud = len("alberto cabrera")
+print("First name length: ", longitud)
+
+# %%
+print(type("first_name"))
+print(type(longitud))
+
+first_name ="alberto"
+logitud =len("alberto cabrera")
+print("first")
+print(type(first_name))
+print(type(logitud))
+
+# %%
+
+primer_nombre,apellido,pais,edad,casado = "alberto", "cabrera", "ecuador", "16"
+print("primer nombre: ",alberto, "apellido: ",cabrera)
+
+
+# %%
