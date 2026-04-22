ddb33928c924e36eb31d542afeb985b756974d86
Alberto Cabrera <e12151@ism.edu.ec>
2026-04-22 07:57:55 -0500

    prueba

--- Prueba.py
+++
@@ -1,5 +0,0 @@
-# %%
-print("hola mundo")
-
-# %% 
-print("francoo")
---
+++ actividad 4 strings.py
@@ -0,0 +1,38 @@
+nombre="python"
+a,b,c,d,e,f=nombre
+print(c,f)
+print(d)
+
+#%%
+
+language="python"
+first=language[0:3]
+print(first)
+
+first=language[-3]
+print(first)
+
+#andate al final y regresa 3
+
+saludo="informatica"
+print(saludo[::-1])
+
+ #%%
+language="python"
+pto=language[0:6:2]#0 da el inicio y 6 
+#l final y dos lo que se va saltando
+print(pto)
+
+challenge="treinta dias en python"
+print(challenge.capitalize())#primera letra mayuscula
+print(challenge.count(e))#cuenta veces que se repite
+
+
+
+
+#print(f se usa para caracteres mas una variable
+
+
+
+
+# %%
